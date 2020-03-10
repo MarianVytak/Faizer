8330646645e7534b43f175a0e5419370b61dd2b3
MarianVytak <marianvytak@gmail.com>
2020-03-10 20:26:44 +0200

    Added Calculation dropdown.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -3,8 +3,34 @@ $(function () {
     'use strict';
 
 
+    // Calculation dropdown
+    let dropdownLink = $('.calculation__route_dropdown_link'),
+        dropdownList = $('.calculation__route_dropdown_list'),
+        dropdownListLink = $('.calculation__route_dropdown_list_item_link'),
+        dropdownLinkText = $('.calculation__route_dropdown_link_text');
 
-    
+    dropdownLink.on('click', function(e) {
+        e.preventDefault();
+        let thisDropdownLink = $(this).attr('href');
+        if (!$(this).hasClass('active')) {
+            dropdownList.slideUp();
+            dropdownLink.removeClass('active');
+        }
+        $(thisDropdownLink).slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    dropdownListLink.on('click', function(e) {
+        e.preventDefault();
+        dropdownLink.removeClass('active');
+        dropdownList.slideUp();
+        let thisDropdownListLink = $(this).attr('href');
+        let thisDropdownListLinkText = $(this).children('span').text();
+        $(thisDropdownListLink).text(thisDropdownListLinkText);
+        let thisListLink = $(this).attr('data-target');
+        $(thisListLink).children().children().removeClass('active');
+        $(this).addClass('active');
+    });
 
 
 
